b35c5506beb1bfe6533c7a04dee48d9563fd6dea
Mohamadi65 <freelancemohamadi65@gmail.com>
2023-02-05 10:21:53 +0100

    presence des fonctions Unique et Not_Null

--- main.py
+++ main.py
@@ -1,16 +1,62 @@
-# This is a sample Python script.
-
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pandas as pd
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def Not_Null(fichier,nom_colonne):
 
 
-# Press the green button in the gutter to run the script.
+
+        for i in range(len(fichier)):
+            # print(format(row.id,'0.0f'), row.nom)
+            valeur = fichier.loc[i, nom_colonne]
+
+            if str(valeur) == 'nan' or str(valeur)=='null':
+                #print("différent de null", format(valeur, '0.0f'))
+
+
+
+                with open("fichier_log.txt", "a", encoding='utf-8') as f:
+
+                        f.write("la colonne ")
+                        f.write("'")
+                        f.write(nom_colonne)
+                        f.write("'")
+                        f.write(" à la ligne ")
+                        f.write(str(i + 2))
+                        f.write(" est vide\n")
+
+
+
+def Unique(fichier,nom_colonne):
+    # déclaration d'une liste pour enrégistrer les données uniques
+    liste=[]  # au début la liste est vide
+    for i in range(len(fichier)):
+        valeur = fichier.loc[i, nom_colonne]
+        liste.append(valeur)
+
+    for i in range(len(fichier)):
+        valeur = fichier.loc[i, nom_colonne]
+        if (str(valeur) != 'nan' or str(valeur)!='null') and liste.count(valeur)>1:
+            if type(valeur)=='int':
+
+                valeur=format(valeur,'0.0f')
+
+
+            with open("fichier_log.txt", "a", encoding='utf-8') as f:
+                f.write("la valeur  : ")
+                f.write(str(valeur))
+                f.write(" qui est presente à la ligne ")
+                f.write(str(i+2))
+                f.write(" de la colonne ")
+                f.write("'")
+                f.write(nom_colonne)
+                f.write("'")
+                f.write(" n'est pas unique\n")
+
+
+
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    fichier = pd.read_csv("C:/code/python/Mouna/Données-Etudiants.csv", header=0, sep=";")
+    Not_Null(fichier,'id_dept')
+    Unique(fichier,'nom')
