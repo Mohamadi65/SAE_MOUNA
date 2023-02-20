66dcde6090e88768b3b12b9ddf396219aba2d601
Mohamadi65 <freelancemohamadi65@gmail.com>
2023-02-20 02:57:52 +0100

    j'ai fini tout et il me reste les comments

--- main.py
+++ main.py
@@ -2,41 +2,64 @@ import pandas as pd
 
 
 
-def Not_Null(fichier,nom_colonne):
+def check_Not_Null(fichier,champ):
 
-
+        retour=0
 
         for i in range(len(fichier)):
-            # print(format(row.id,'0.0f'), row.nom)
-            valeur = fichier.loc[i, nom_colonne]
+
+            valeur = fichier.loc[i, champ]
 
             if str(valeur) == 'nan' or str(valeur)=='null':
-                #print("différent de null", format(valeur, '0.0f'))
+
 
 
 
                 with open("fichier_log.txt", "a", encoding='utf-8') as f:
 
-                        f.write("la colonne ")
-                        f.write("'")
-                        f.write(nom_colonne)
-                        f.write("'")
-                        f.write(" à la ligne ")
-                        f.write(str(i + 2))
-                        f.write(" est vide alors qu'il ne doit pas l'être \n")
+                    retour = retour + 1
+                    if retour == 1:
+                        with open("fichier_log.txt", "a", encoding='utf-8') as f:
+                            f.write("\nvérification de la contrainte Null sur la colonne : ")
+
+                            f.write("'")
+                            f.write(champ)
+
+                            f.write("'")
+                            f.write(" \n")
+                            f.write("ligne ")
+                            f.write(str(i + 2))
+                            f.write(", ")
+                            f.write("la valeur ")
+                            f.write("'")
+                            f.write(str(valeur))
+                            f.write("'")
+                            f.write("")
+                            f.write(" est vide \n")
+                    else:
+                        with open("fichier_log.txt", "a", encoding='utf-8') as f:
+                            f.write("ligne ")
+                            f.write(str(i + 2))
+                            f.write(", ")
+                            f.write("la valeur ")
+                            f.write("'")
+                            f.write(str(valeur))
+                            f.write("'")
+                            f.write("")
+                            f.write(" est vide \n")
 
 
 
-def Unique(fichier,nom_colonne):
-    
+def check_Unique(fichier,champ):
+    retour=0
     # déclaration d'une liste pour enrégistrer les données uniques
     liste=[]  # au début la liste est vide
     for i in range(len(fichier)):
-        valeur = fichier.loc[i, nom_colonne]
+        valeur = fichier.loc[i, champ]
         liste.append(valeur)
 
     for i in range(len(fichier)):
-        valeur = fichier.loc[i, nom_colonne]
+        valeur = fichier.loc[i, champ]
         if (str(valeur) != 'nan' or str(valeur)!='null') and liste.count(valeur)>1:
             if type(valeur)=='int':
 
@@ -44,28 +67,48 @@ def Unique(fichier,nom_colonne):
 
 
             with open("fichier_log.txt", "a", encoding='utf-8') as f:
-                f.write("la valeur  : ")
-                f.write(str(valeur))
-                f.write(" qui est presente à la ligne ")
-                f.write(str(i+2))
-                f.write(" de la colonne ")
-                f.write("'")
-                f.write(nom_colonne)
-                f.write("'")
-                f.write(" n'est pas unique , ce qui contrarie la contrainte d'unicité de la colonne\n")
+                retour = retour + 1
+                if retour == 1:
+                    with open("fichier_log.txt", "a", encoding='utf-8') as f:
+                        f.write("\nvérification de la contrainte clé étrangère sur la colonne : ")
+
+                        f.write("'")
+                        f.write(champ)
+
+                        f.write("'")
+                        f.write(" \n")
+                        f.write("ligne ")
+                        f.write(str(i + 2))
+                        f.write(", ")
+                        f.write("la valeur ")
+                        f.write("'")
+                        f.write(str(valeur))
+                        f.write("'")
+                        f.write("")
+                        f.write(" n'est pas unique \n")
+                else:
+                    with open("fichier_log.txt", "a", encoding='utf-8') as f:
+                        f.write("ligne ")
+                        f.write(str(i + 2))
+                        f.write(", ")
+                        f.write("la valeur ")
+                        f.write("'")
+                        f.write(str(valeur))
+                        f.write("'")
+                        f.write("")
+                        f.write(" n'est pas unique \n")
 
 
 
-def primary_key(fichier,nomcolonne):
-    with open("fichier_log.txt", "a", encoding='utf-8') as f:
-        f.write("\n\n")
-        unicite =Unique(fichier,nomcolonne)
-        if unicite:
-            f.write("cool")
-        vide=Not_Null(fichier,nomcolonne)
-        f.write("\n\n")
+def check_primary_key(fichier,champ):
+    if str(champ)=="id":
+        check_Unique(fichier,champ)
 
-def foreign_key(etudiant,departement,col_etu,col_dept):
+        check_Not_Null(fichier,champ)
+
+
+def check_foreign_key(etudiant,departement,col_etu,col_dept):
+    retour=0
     liste_dept=[]
     #récuperons tous les numéros des département et mettons les dans une liste
     for i in range(len(departement)) :
@@ -75,27 +118,48 @@ def foreign_key(etudiant,departement,col_etu,col_dept):
     #Ici notre liste contient la liste des département
 
     for i in range(len(etudiant)):
-        valeur = fichier.loc[i, col_etu]
-        print(type(valeur))
+        valeur = etudiant.loc[i, col_etu]
+
         if valeur not in liste_dept:
-            print("not correspond",valeur)
+
             with open("fichier_log.txt", "a", encoding='utf-8') as f:
-                f.write("colonne ")
-                f.write("' ")
-                f.write(col_etu)
-                f.write("' ")
-                f.write("la valeur  : ")
-                f.write(str(valeur))
-                f.write(" ligne :")
-                f.write(str(i + 2))
-                f.write("'")
-                f.write(" n'est pas une clé étrangère \n")
+                retour = retour + 1
+                if retour == 1:
+                    with open("fichier_log.txt", "a", encoding='utf-8') as f:
+                        f.write("\nvérification de la contrainte clé étrangère sur la colonne : ")
+
+                        f.write("'")
+                        f.write(col_etu)
+
+                        f.write("'")
+                        f.write(" \n")
+                        f.write("ligne ")
+                        f.write(str(i + 2))
+                        f.write(", ")
+                        f.write("la valeur ")
+                        f.write("'")
+                        f.write(str(valeur))
+                        f.write("'")
+                        f.write("")
+                        f.write(" n'est pas une clé étrangère \n")
+                else:
+                    with open("fichier_log.txt", "a", encoding='utf-8') as f:
+                        f.write("ligne ")
+                        f.write(str(i + 2))
+                        f.write(", ")
+                        f.write("la valeur ")
+                        f.write("'")
+                        f.write(str(valeur))
+                        f.write("'")
+                        f.write("")
+
+                        f.write(" n'est pas une clé étrangère \n")
 
 
 
 
 def check_type(fichier,champ):
-
+    retour=0
 
     if str(champ)=='id' or str(champ)=="id_dept":
         for i in range(len(fichier)):
@@ -105,41 +169,74 @@ def check_type(fichier,champ):
                     valeur=float(valeur)
                     if not valeur.is_integer():
 
+                        retour = retour + 1
+                        if retour == 1:
+                            with open("fichier_log.txt", "a", encoding='utf-8') as f:
+                                f.write("\nvérification de la contrainte type sur la colonne : ")
 
+                                f.write("'")
+                                f.write(champ)
+
+                                f.write("'")
+                                f.write(" \n")
+                                f.write("ligne ")
+                                f.write(str(i + 2))
+                                f.write(", ")
+                                f.write("la valeur ")
+                                f.write("'")
+                                f.write(str(valeur))
+                                f.write("'")
+                                f.write("")
+                                f.write(" n'est pas un entier \n")
+                        else:
+                            with open("fichier_log.txt", "a", encoding='utf-8') as f:
+                                f.write("ligne ")
+                                f.write(str(i + 2))
+                                f.write(", ")
+                                f.write("la valeur ")
+                                f.write("'")
+                                f.write(str(valeur))
+                                f.write("'")
+                                f.write("")
+                                f.write(" n'est pas un entier \n")
+                except:
+                    retour = retour + 1
+                    if retour == 1:
                         with open("fichier_log.txt", "a", encoding='utf-8') as f:
-                            f.write("colonne ")
-                            f.write("' ")
-                            f.write(champ)
-                            f.write("' ")
-                            f.write("la valeur  : ")
-                            f.write(str(valeur))
-                            f.write(" ligne :")
-                            f.write(str(i + 2))
+                            f.write("\nvérification de la contrainte check sur la colonne : ")
 
                             f.write("'")
+                            f.write(champ)
+
+                            f.write("'")
+                            f.write(" \n")
+                            f.write("ligne ")
+                            f.write(str(i + 2))
+                            f.write(", ")
+                            f.write("la valeur ")
+                            f.write("'")
+                            f.write(str(valeur))
+                            f.write("'")
+                            f.write("")
+                            f.write(" n'est pas un entier \n")
+                    else:
+                        with open("fichier_log.txt", "a", encoding='utf-8') as f:
+                            f.write("ligne ")
+                            f.write(str(i + 2))
+                            f.write(", ")
+                            f.write("la valeur ")
+                            f.write("'")
+                            f.write(str(valeur))
+                            f.write("'")
+                            f.write("")
                             f.write(" n'est pas un entier \n")
 
-                except:
-                    with open("fichier_log.txt", "a", encoding='utf-8') as f:
-                        f.write("colonne ")
-                        f.write("' ")
-                        f.write(champ)
-                        f.write("' ")
-                        f.write("la valeur  : ")
-                        f.write(str(valeur))
-                        f.write(" ligne :")
-                        f.write(str(i + 2))
-
-                        f.write("'")
-                        f.write(" n'est pas un entier \n")
 
 
 
 
 
 
-
-            # je vérifie que la valeur n'est pas un entier
     else:
         if  str(champ)=="nom" or str(champ)=="prenom":
             for i in range(len(fichier)):
@@ -168,68 +265,109 @@ def check_type(fichier,champ):
 
 
 def check_taille(fichier,champ):
+    retour=0
     if str(champ) == "nom" or str(champ) == "prenom" or str(champ) == "mdp" or str(champ) == "login" :
         for i in range(len(fichier)):
             valeur = fichier.loc[i, champ]
-            if len(valeur)>50:
-                with open("fichier_log.txt", "a", encoding='utf-8') as f:
-                    f.write("colonne ")
-                    f.write("' ")
-                    f.write(champ)
-                    f.write("' ")
-                    f.write("la valeur  : ")
-                    f.write(str(valeur))
-                    f.write(" ligne :")
-                    f.write(str(i + 2))
+            if len(str(valeur))>50:
+                retour = retour + 1
+                if retour == 1:
+                    with open("fichier_log.txt", "a", encoding='utf-8') as f:
+                        f.write("\nvérification de la taille sur la colonne : ")
 
-                    f.write("'")
-                    f.write(" à une taille supèrieur à 50 \n")
+                        f.write("'")
+                        f.write(champ)
+
+                        f.write("'")
+                        f.write(" \n")
+                        f.write("ligne ")
+                        f.write(str(i + 2))
+                        f.write(", ")
+                        f.write("la valeur")
+                        f.write("'")
+                        f.write(str(valeur))
+                        f.write("'")
+                        f.write("")
+                        f.write(" dépasse 50 caractère \n")
+                else:
+                    with open("fichier_log.txt", "a", encoding='utf-8') as f:
+                        f.write("ligne ")
+                        f.write(str(i + 2))
+                        f.write(", ")
+                        f.write("la valeur ")
+                        f.write("'")
+                        f.write(str(valeur))
+                        f.write("'")
+                        f.write("")
+                        f.write(" dépasse 50 caractère \n")
 
 def check_check(fichier,champ):
+    retour=0
     if str(champ) == "mdp" :
         for i in range(len(fichier)):
             valeur = fichier.loc[i, champ]
             if len(valeur)<8:
-                with open("fichier_log.txt", "a", encoding='utf-8') as f:
-                    f.write("colonne ")
-                    f.write("' ")
-                    f.write(champ)
-                    f.write("' ")
-                    f.write("la valeur  : ")
-                    f.write(str(valeur))
-                    f.write(" ligne :")
-                    f.write(str(i + 2))
+                retour=retour+1
+                if retour==1:
+                    with open("fichier_log.txt", "a", encoding='utf-8') as f:
+                        f.write("\nvérification de la contrainte check sur la colonne : ")
 
-                    f.write("'")
-                    f.write(" doit avoir une taille >=8 \n")
+                        f.write("'")
+                        f.write(champ)
+
+                        f.write("'")
+                        f.write(" \n")
+                        f.write("ligne ")
+                        f.write(str(i + 2))
+                        f.write(", ")
+                        f.write("la valeur ")
+                        f.write("'")
+                        f.write(str(valeur))
+                        f.write("'")
+                        f.write("")
+                        f.write(" doit avoir une taille >=8 \n")
+                else:
+                    with open("fichier_log.txt", "a", encoding='utf-8') as f:
+                        f.write("ligne ")
+                        f.write(str(i + 2))
+                        f.write(", ")
+                        f.write("la valeur ")
+                        f.write("'")
+                        f.write(str(valeur))
+                        f.write("'")
+                        f.write("")
+                        f.write(" doit avoir une taille >=8 \n")
 
 
 if __name__ == '__main__':
 
-    # définition de la structure de données
-
-    database_schema=[ {"nom_champ":"id","type":"integer","contrainte":['notnull','unique']},
-               {"nom_champ":"nom","type":"character","taille":50,"contrainte": ['notnull']},
-               {"nom_champ":"prenom","type":"character","taille":50,"contrainte": ['notnull',"unique"]},
-               {"nom_champ": "login", "type": "character", "taille": 50, "contrainte": ['notnull', "unique"]},
-               {"nom_champ": "mdp", "type": "character", "taille": 50, "contrainte": {'notnull':"true", "unique":"true","check":{"min":8}}},
-               {"nom_champ": "id_dept", "type": "integer"}
-               ]
-
-
-
-    fichier = pd.read_csv("Données-Etudiants.csv", header=0, sep=";",decimal=".")
+    #importation des deux fichiers
+    etudiant = pd.read_csv("Données-Etudiants.csv", header=0, sep=";",decimal=".")
     departement=pd.read_csv("Données-Departements.csv",header=0, sep=";")
-    """Not_Null(fichier,'id_dept')
-    Unique(fichier,'nom')
-    primary_key(fichier,'id')
-    """
-    #check_type(fichier,'id')
-    #check_type(fichier,'nom')
-    foreign_key(fichier,departement,'id_dept','num_dept')
-    #import mon_programme as p
-    #print(p.test())
-    #check_type(fichier,'login')
-    #check_type(fichier,'id')
-    check_check(fichier,'mdp')
+
+    # Appel des fonctions
+    check_Not_Null(etudiant,'id')
+    check_Not_Null(etudiant,'nom')
+    check_Not_Null(etudiant,'prenom')
+    check_Not_Null(etudiant,'login')
+    check_Not_Null(etudiant,'mdp')
+    check_type(etudiant,"id")
+    check_type(etudiant,"id_dept")
+    check_type(etudiant,"nom")
+    check_type(etudiant,"prenom")
+    check_Unique(etudiant,'login')
+    check_Unique(etudiant,'mdp')
+    check_primary_key(etudiant,'id')
+    check_taille(etudiant,'nom')
+    check_taille(etudiant,'prenom')
+    check_taille(etudiant,'login')
+    check_taille(etudiant,'mdp')
+    check_check(etudiant,'mdp')
+    check_foreign_key(etudiant,departement,'id_dept','num_dept')
+
+
+
+
+
+
 
